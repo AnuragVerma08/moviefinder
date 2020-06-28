b365b263234b3131692578a3c6401e52566561b2
Astarexz <anurockaples@gmail.com>
2020-06-28 16:57:47 +0530

    Updates

---
+++ precache-manifest.fe0fec6389cfec011666a34042a3d010.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0d29fc64662773051f9a20452a5125b",
+    "url": "/moviefinder/index.html"
+  },
+  {
+    "revision": "95b5e902b9350cf1c927",
+    "url": "/moviefinder/static/css/2.ec6da10f.chunk.css"
+  },
+  {
+    "revision": "7e7a55db862d9ff7d8a1",
+    "url": "/moviefinder/static/css/main.64f44ac2.chunk.css"
+  },
+  {
+    "revision": "95b5e902b9350cf1c927",
+    "url": "/moviefinder/static/js/2.53ead018.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/moviefinder/static/js/2.53ead018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e7a55db862d9ff7d8a1",
+    "url": "/moviefinder/static/js/main.00d69c3b.chunk.js"
+  },
+  {
+    "revision": "1498b0f03b2ef508fcd4",
+    "url": "/moviefinder/static/js/runtime-main.daf8c32d.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/moviefinder/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/moviefinder/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/moviefinder/static/media/revicons.2feb69cc.eot"
+  }
+]);
--- src/components/MovieCard/MovieCardStates.ts
+++
@@ -1,3 +0,0 @@
-export interface MovieCardStates {
-    rating: number;
-}
